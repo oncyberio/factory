0a3ee7376f03780035d3466b6bf7f761a47203d3
julesGoullee <julesgoullee@gmail.com>
2021-05-27 11:49:56 +0400

    jsonContract

--- packages/front-end/utils/minter.js
+++ packages/front-end/utils/minter.js
@@ -1,15 +1,15 @@
 // @ts-ignore
 import * as Web3 from 'web3'
 import { ethers } from 'ethers';
-import jsonContract from './contract.json';
+import jsonContract from '../../contracts/deployments/mumbai/DiamondCyber.json';
 
 async function mint(uri, amount, signature) {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const minter = provider.getSigner();
 
   const contract = new ethers.Contract(
-    jsonContract,
-    "0xFBe0421c53706746151ACa2Cf22F81Dc41262519",
+    jsonContract.address,
+    jsonContract.abi,
     minter
   )
 
@@ -18,7 +18,7 @@ async function mint(uri, amount, signature) {
   await tx.wait()
 
   const balanceMinter = await contract.balanceOf(minter.address, 0)
-  
+
   return balanceMinter;
 }
 
