a793bed3c4b113d894ad2f971be7a0cbba6c2b23
iamsupercharged <rayan@oncyber.io>
2021-05-28 08:22:15 +0100

    Delete server folder

--- packages/server/api/ping.ts
+++
@@ -1,9 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default (request: VercelRequest, response: VercelResponse) => {
-  if (request.method === 'OPTIONS') {
-    return response.status(200).end()
-  }
-
-  response.status(200).send(`pong ${Date.now()}`)
-}
