5c5c7fe6b3a7f589f72c54247f9aea058d4c117f
iamsupercharged <rayan@oncyber.io>
2021-05-28 07:34:43 +0100

    commit to push

---
+++ packages/front-end/components/Listing.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+function Listing({token_id}) {
+    return <Container>
+    </Container>
+}
+
+const Container = styled.div`
+
+`
+
+export default Listing;
