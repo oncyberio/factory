72defd80a1486db9c269415fe3de842521631272
jules goullée <julesgoullee@gmail.com>
2021-06-13 21:30:55 +0700

    Init utility (#14)
    * init utility
    
    * script
    
    * New contrats
    
    * signer
    
    * managerPrivateKey
    
    * managerPrivateKey
    
    Co-authored-by: rayanoncyber <rayan@oncyber.io>

---
+++ packages/contracts/scripts/initialize.ts
@@ -0,0 +1,33 @@
+// @ts-ignore
+import hre, { ethers } from 'hardhat'
+
+async function main() {
+  const contractName = 'DiamondCyberDestinationUtilityFactory'
+  const { deployments, getNamedAccounts} = hre
+  const namedAccounts = await getNamedAccounts()
+  const accounts = await ethers.getSigners()
+  const minter = accounts[0]
+
+  const Contract = await deployments.get(contractName)
+  const contract = await ethers.getContractAt(
+    Contract.abi,
+    Contract.address,
+    minter
+  )
+
+  const tx = await contract.initialize(
+    'ipfs://',
+    namedAccounts.managerDestinationUtility,
+    namedAccounts.biconomyForwarder,
+    namedAccounts.opensea,
+    namedAccounts.oncyber
+  )
+  console.log('tx', tx)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
