1af99c3e1200b6c7bbba312df1cf2bdb43199589
julesGoullee <julesgoullee@gmail.com>
2021-06-13 17:55:48 +0400

    init utility

--- packages/contracts/deploy/002CyberDestinationUtilityFactory.ts
+++ packages/contracts/deploy/002CyberDestinationUtilityFactory.ts
@@ -1,27 +1,43 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
+import {ethers} from "hardhat";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
-  const { diamond } = deployments
+  // const { diamond } = deployments
 
   const namedAccounts = await getNamedAccounts()
-  await diamond.deploy('DiamondCyberDestinationUtilityFactory', {
-    from: namedAccounts.deployer,
-    owner: namedAccounts.deployer,
-    facets: ['CyberDestinationUtilityFactoryFacet'],
-    execute: {
-      methodName: 'initialize',
-      args: [
-        'ipfs://',
-        namedAccounts.managerDestinationUtility,
-        namedAccounts.biconomyForwarder,
-        namedAccounts.opensea,
-        namedAccounts.oncyber
-      ],
-    },
-    log: true,
-  })
+  const Contract = await deployments.get('DiamondCyberDestinationUtilityFactory')
+  const accounts = await ethers.getSigners()
+  const minter = accounts[0]
+  const contract = await ethers.getContractAt(
+    Contract.abi,
+    Contract.address,
+    minter
+  )
+  await contract.initialize(
+    'ipfs://',
+    namedAccounts.managerDestinationUtility,
+    namedAccounts.biconomyForwarder,
+    namedAccounts.opensea,
+    namedAccounts.oncyber
+  )
+  // await diamond.deploy('DiamondCyberDestinationUtilityFactory', {
+  //   from: namedAccounts.deployer,
+  //   owner: namedAccounts.deployer,
+  //   facets: ['CyberDestinationUtilityFactoryFacet'],
+  //   execute: {
+  //     methodName: 'initialize',
+  //     args: [
+  //       'ipfs://',
+  //       namedAccounts.managerDestinationUtility,
+  //       namedAccounts.biconomyForwarder,
+  //       namedAccounts.opensea,
+  //       namedAccounts.oncyber
+  //     ],
+  //   },
+  //   log: true,
+  // })
 }
 export default func
 func.tags = ['DiamondCyberDestinationUtilityFactory']
