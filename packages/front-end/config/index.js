4b67f1b7f1f6a374303ee60d2ff3c9d7c9208a3c
rayanoncyber <rayan@oncyber.io>
2021-06-21 15:21:02 -0400

    Config with prod

--- packages/front-end/config/index.js
+++ packages/front-end/config/index.js
@@ -2,7 +2,7 @@
 console.log("MIN ONCYBER SHARES")
 console.log(process.env.NEXT_PUBLIC_MIN_ONCYBER_SHARES)
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NEXT_PUBLIC_NODE_ENV || 'development',
   supportedContracts: process.env.SUPPORTED_CONTRACTS ? process.env.SUPPORTED_CONTRACTS.split(',') : ['destination', 'destinationUtility'],
   destination: {
     allowedMinter: process.env.DESTINATION_ALLOWED_MINTER_ADDRESS ? process.env.DESTINATION_ALLOWED_MINTER_ADDRESS.split(',') : ['0xBeb171bA24e66014F356Bca0DB05329EFea14964', '0x76DB02500F7631d57BC2DcDCa9d4cf782b99E119'],
