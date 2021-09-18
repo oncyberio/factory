05ea3e408eaad2ec852572e6f3daaffb7a239786
rayanoncyber <rayan@oncyber.io>
2021-09-18 13:43:23 +0100

    Delete console log

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -13,7 +13,6 @@ import { parseUnits } from 'ethers/lib/utils'
 
 const defaultPrivateKey =
   '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80' // hardhat 0
-console.log("GWEI, " + parseUnits('65', 'gwei').toNumber());
 
 const config: HardhatUserConfig = {
   preprocess: {
