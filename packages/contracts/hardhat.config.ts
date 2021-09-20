9a9f095b315a35a5e6ffe70f5776ce02b66b2ed2
rayanoncyber <rayan@oncyber.io>
2021-09-20 18:39:23 +0100

    straightforward verification for previous contract

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -41,6 +41,24 @@ const config: HardhatUserConfig = {
           },
         },
       },
+      {
+        version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: '0.7',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
     ],
   },
   namedAccounts: {
