42fcae6e1ccd0d62f7da128e03aa000a490603e1
julesGoullee <julesgoullee@gmail.com>
2021-11-11 16:40:52 +0100

    .prettierignore

--- packages/contracts/typechain-types/factories/BaseRelayRecipientStorage__factory.ts
+++ packages/contracts/typechain-types/factories/BaseRelayRecipientStorage__factory.ts
@@ -2,69 +2,69 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import { Signer, utils, Contract, ContractFactory, Overrides } from 'ethers'
-import { Provider, TransactionRequest } from '@ethersproject/providers'
+import { Signer, utils, Contract, ContractFactory, Overrides } from "ethers";
+import { Provider, TransactionRequest } from "@ethersproject/providers";
 import type {
   BaseRelayRecipientStorage,
   BaseRelayRecipientStorageInterface,
-} from '../BaseRelayRecipientStorage'
+} from "../BaseRelayRecipientStorage";
 
 const _abi = [
   {
     inputs: [
       {
-        internalType: 'bytes32',
-        name: 'c__0x08a8f2bf',
-        type: 'bytes32',
+        internalType: "bytes32",
+        name: "c__0x08a8f2bf",
+        type: "bytes32",
       },
     ],
-    name: 'c_0x08a8f2bf',
+    name: "c_0x08a8f2bf",
     outputs: [],
-    stateMutability: 'pure',
-    type: 'function',
+    stateMutability: "pure",
+    type: "function",
   },
-]
+];
 
 const _bytecode =
-  '0x60e7610052600b82828239805160001a607314610045577f4e487b7100000000000000000000000000000000000000000000000000000000600052600060045260246000fd5b30600052607381538281f3fe730000000000000000000000000000000000000000301460806040526004361060335760003560e01c8063d1c07207146038575b600080fd5b604e6004803603810190604a91906089565b6050565b005b50565b600080fd5b6000819050919050565b6069816058565b8114607357600080fd5b50565b6000813590506083816062565b92915050565b600060208284031215609c57609b6053565b5b600060a8848285016076565b9150509291505056fea2646970667358221220a1d15ab3fedd720e2d73ff11a3d81610c174034a449c89aebfb086b9b265bb7664736f6c63430008090033'
+  "0x60e7610052600b82828239805160001a607314610045577f4e487b7100000000000000000000000000000000000000000000000000000000600052600060045260246000fd5b30600052607381538281f3fe730000000000000000000000000000000000000000301460806040526004361060335760003560e01c8063d1c07207146038575b600080fd5b604e6004803603810190604a91906089565b6050565b005b50565b600080fd5b6000819050919050565b6069816058565b8114607357600080fd5b50565b6000813590506083816062565b92915050565b600060208284031215609c57609b6053565b5b600060a8848285016076565b9150509291505056fea2646970667358221220a1d15ab3fedd720e2d73ff11a3d81610c174034a449c89aebfb086b9b265bb7664736f6c63430008090033";
 
 type BaseRelayRecipientStorageConstructorParams =
   | [signer?: Signer]
-  | ConstructorParameters<typeof ContractFactory>
+  | ConstructorParameters<typeof ContractFactory>;
 
 const isSuperArgs = (
   xs: BaseRelayRecipientStorageConstructorParams
-): xs is ConstructorParameters<typeof ContractFactory> => xs.length > 1
+): xs is ConstructorParameters<typeof ContractFactory> => xs.length > 1;
 
 export class BaseRelayRecipientStorage__factory extends ContractFactory {
   constructor(...args: BaseRelayRecipientStorageConstructorParams) {
     if (isSuperArgs(args)) {
-      super(...args)
+      super(...args);
     } else {
-      super(_abi, _bytecode, args[0])
+      super(_abi, _bytecode, args[0]);
     }
   }
 
   deploy(
     overrides?: Overrides & { from?: string | Promise<string> }
   ): Promise<BaseRelayRecipientStorage> {
-    return super.deploy(overrides || {}) as Promise<BaseRelayRecipientStorage>
+    return super.deploy(overrides || {}) as Promise<BaseRelayRecipientStorage>;
   }
   getDeployTransaction(
     overrides?: Overrides & { from?: string | Promise<string> }
   ): TransactionRequest {
-    return super.getDeployTransaction(overrides || {})
+    return super.getDeployTransaction(overrides || {});
   }
   attach(address: string): BaseRelayRecipientStorage {
-    return super.attach(address) as BaseRelayRecipientStorage
+    return super.attach(address) as BaseRelayRecipientStorage;
   }
   connect(signer: Signer): BaseRelayRecipientStorage__factory {
-    return super.connect(signer) as BaseRelayRecipientStorage__factory
+    return super.connect(signer) as BaseRelayRecipientStorage__factory;
   }
-  static readonly bytecode = _bytecode
-  static readonly abi = _abi
+  static readonly bytecode = _bytecode;
+  static readonly abi = _abi;
   static createInterface(): BaseRelayRecipientStorageInterface {
-    return new utils.Interface(_abi) as BaseRelayRecipientStorageInterface
+    return new utils.Interface(_abi) as BaseRelayRecipientStorageInterface;
   }
   static connect(
     address: string,
@@ -74,6 +74,6 @@ export class BaseRelayRecipientStorage__factory extends ContractFactory {
       address,
       _abi,
       signerOrProvider
-    ) as BaseRelayRecipientStorage
+    ) as BaseRelayRecipientStorage;
   }
 }
