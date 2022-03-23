a8419dfd3e3d41e7cdaba4329cf53e9f5363b86e
Rayan Boutaleb <rayan@oncyber.io>
2022-03-23 13:28:18 +0100

    Re-write contract + utils, tests to be done

--- packages/contracts/contracts/CyberDropBase.sol
+++ packages/contracts/contracts/CyberDropBase.sol
@@ -21,7 +21,6 @@ contract CyberDropBase is CyberTokenBase {
     returns (uint256)
   {
     LibDropStorage.Drop storage drop = LibDropStorage.layout().drops[_tokenId];
-    require(drop.priceStart != 0, 'DNE');
     return drop.mintCounter[_minter].current();
   }
 
@@ -31,9 +30,7 @@ contract CyberDropBase is CyberTokenBase {
     returns (
       uint256 timeStart,
       uint256 timeEnd,
-      uint256 priceStart,
-      uint256 priceEnd,
-      uint256 stepDuration,
+      uint256 price,
       uint256 amountCap,
       uint256 shareCyber,
       address creator,
@@ -41,14 +38,11 @@ contract CyberDropBase is CyberTokenBase {
     )
   {
     LibDropStorage.Drop storage drop = LibDropStorage.layout().drops[_tokenId];
-    require(drop.priceStart != 0, 'DNE');
 
     return (
       drop.timeStart,
       drop.timeEnd,
-      drop.priceStart,
-      drop.priceEnd,
-      drop.stepDuration,
+      drop.price,
       drop.amountCap,
       drop.shareCyber,
       drop.creator,
@@ -60,15 +54,13 @@ contract CyberDropBase is CyberTokenBase {
     string memory _uri,
     uint256 _timeStart,
     uint256 _timeEnd,
-    uint256 _priceStart,
-    uint256 _priceEnd,
-    uint256 _stepDuration,
+    uint256 _price,
     uint256 _amountCap,
     uint256 _shareCyber,
     bytes memory _signature
   ) public returns (uint256 tokenId) {
-    require(_timeEnd - _timeStart >= _stepDuration && _stepDuration > 0, 'IT');
-    require(_priceStart >= _priceEnd && _priceStart > 0, 'IP');
+    require(_timeEnd - _timeStart >= 0, 'IT');
+    require(_price >= 0, 'IP');
     require(_shareCyber <= 100, 'ISO');
 
     address sender = _msgSender();
@@ -77,9 +69,7 @@ contract CyberDropBase is CyberTokenBase {
       _uri,
       _timeStart,
       _timeEnd,
-      _priceStart,
-      _priceEnd,
-      _stepDuration,
+      _price,
       _amountCap,
       _shareCyber,
       sender,
@@ -94,27 +84,22 @@ contract CyberDropBase is CyberTokenBase {
     // Effects
     setTokenURI(tokenId, _uri);
     LibAppStorage.layout().totalSupply.increment();
-    LibAppStorage.layout().minterNonce[sender].increment();
+
+    // this is if the user mints, not if the user just creates drop
+    // LibAppStorage.layout().minterNonce[sender].increment();
 
     LibDropStorage.layout().drops[tokenId].timeStart = _timeStart;
     LibDropStorage.layout().drops[tokenId].timeEnd = _timeEnd;
-    LibDropStorage.layout().drops[tokenId].priceStart = _priceStart;
-    LibDropStorage.layout().drops[tokenId].priceEnd = _priceEnd;
-    LibDropStorage.layout().drops[tokenId].stepDuration = _stepDuration;
+    LibDropStorage.layout().drops[tokenId].price = _price;
     LibDropStorage.layout().drops[tokenId].amountCap = _amountCap;
     LibDropStorage.layout().drops[tokenId].shareCyber = _shareCyber;
     LibDropStorage.layout().drops[tokenId].creator = payable(sender);
 
-    // Mint for creator
-    LibDropStorage.layout().drops[tokenId].minted.increment();
-    LibDropStorage.layout().drops[tokenId].mintCounter[sender].increment();
-    _safeMint(sender, tokenId, 1, '');
-
     emit DropCreated(sender, tokenId);
-    emit Minted(sender, tokenId, 1);
+    return tokenId;
   }
 
-  function mint(uint256 _tokenId, bytes memory _signature)
+  function mint(uint256 _tokenId, uint256 _quantity, bytes memory _signature)
     public
     payable
     returns (bool success)
@@ -124,27 +109,18 @@ contract CyberDropBase is CyberTokenBase {
 
     if (drop.amountCap != 0) {
       require(drop.minted.current() < drop.amountCap, 'CR');
+      require(drop.amountCap - drop.minted.current() >= _quantity, 'CR');
     }
 
     require(
       block.timestamp > drop.timeStart && block.timestamp <= drop.timeEnd,
       'OOT'
     );
-    uint256 timeSpent = block.timestamp - drop.timeStart;
-    uint256 duration = drop.timeEnd - drop.timeStart;
-    uint256 price = getPriceFor(
-      timeSpent,
-      duration,
-      drop.priceStart,
-      drop.priceEnd,
-      drop.stepDuration
-    );
-    require(msg.value >= price, 'IA');
-    uint256 amountOnCyber = (msg.value * drop.shareCyber) / 100;
-    uint256 amountCreator = msg.value - amountOnCyber;
+
+    require(msg.value >= drop.price, 'IA');
 
     uint256 senderDropNonce = drop.mintCounter[sender].current();
-    bytes memory _message = abi.encodePacked(_tokenId, sender, senderDropNonce);
+    bytes memory _message = abi.encodePacked(_tokenId, _quantity, sender, senderDropNonce);
     address recoveredAddress = keccak256(_message)
       .toEthSignedMessageHash()
       .recover(_signature);
@@ -153,60 +129,19 @@ contract CyberDropBase is CyberTokenBase {
     // Effects
     drop.minted.increment();
     drop.mintCounter[sender].increment();
-    _safeMint(sender, _tokenId, 1, '');
-    drop.creator.transfer(amountCreator);
-    payable(LibAppStorage.layout().oncyber).transfer(amountOnCyber);
+    _safeMint(sender, _tokenId, _quantity, '');
 
-    emit Minted(sender, _tokenId, 1);
+    if (drop.price > 0) {
+      uint256 amountOnCyber = (msg.value * drop.shareCyber) / 100;
+      uint256 amountCreator = msg.value - amountOnCyber;
+      
+      drop.creator.transfer(amountCreator);
+      payable(LibAppStorage.layout().oncyber).transfer(amountOnCyber);
+    }
+
+    emit Minted(sender, _tokenId, _quantity);
 
     return true;
   }
 
-  function getMintPriceForToken(uint256 _tokenId)
-    public
-    view
-    returns (uint256 mintPrice)
-  {
-    LibDropStorage.Drop storage drop = LibDropStorage.layout().drops[_tokenId];
-    require(drop.priceStart != 0, 'DNE');
-
-    if (drop.amountCap != 0) {
-      require(drop.minted.current() < drop.amountCap, 'CR');
-    }
-
-    require(
-      block.timestamp > drop.timeStart && block.timestamp <= drop.timeEnd,
-      'OOT'
-    );
-    uint256 timeSpent = block.timestamp - drop.timeStart;
-    uint256 duration = drop.timeEnd - drop.timeStart;
-
-    return
-      getPriceFor(
-        timeSpent,
-        duration,
-        drop.priceStart,
-        drop.priceEnd,
-        drop.stepDuration
-      );
-  }
-
-  function getPriceFor(
-    uint256 _timeSpent,
-    uint256 _duration,
-    uint256 _priceStart,
-    uint256 _priceEnd,
-    uint256 _stepDuration
-  ) public pure returns (uint256 price) {
-    // https://www.desmos.com/calculator/oajpdvew5q
-    // f\left(x\right)=\frac{s\ \cdot d\ +\ \operatorname{mod}\left(x,\ g\right)\ \cdot\ \left(s\ -\ l\right)\ -\ x\ \cdot\ \left(s\ -\ l\right)\ \ }{d}
-    // (s * d + (x % g) * (s - l) - x * (s - l) / d
-    return
-      (_duration *
-        _priceStart +
-        (_timeSpent % _stepDuration) *
-        (_priceStart - _priceEnd) -
-        _timeSpent *
-        (_priceStart - _priceEnd)) / _duration;
-  }
 }
