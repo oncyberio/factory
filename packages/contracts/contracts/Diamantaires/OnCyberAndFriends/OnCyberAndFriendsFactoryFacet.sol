5b19d5352713a656b2a831f6db17cc4875f6eaa1
Rayan Boutaleb <rayan@oncyber.io>
2022-08-31 13:12:31 +0100

    Add coverage

--- packages/contracts/contracts/Diamantaires/OnCyberAndFriends/OnCyberAndFriendsFactoryFacet.sol
+++ packages/contracts/contracts/Diamantaires/OnCyberAndFriends/OnCyberAndFriendsFactoryFacet.sol
@@ -14,13 +14,15 @@ contract OnCyberAndFriendsFactoryFacet is CyberDropBase {
     uint256 amount
   ) external virtual {
     uint256 tokenId = id + 14;
-    require(tokenId == 14 || tokenId == 15, 'NT');
+    require((id == 0 || id == 1) && (tokenId == 14 || tokenId == 15), 'NT');
 
     address sender = _msgSender();
     require(sender == 0xBf0562B81eF8E52316BDAF6f36948Fd8bE82fCC8, 'NS');
 
     LibDropStorage.Drop storage drop = LibDropStorage.layout().drops[tokenId];
     require(drop.amountCap - drop.minted >= amount, 'CR');
+    require(block.timestamp > drop.timeStart && block.timestamp <= drop.timeEnd, 'OOT');
+
 
     // Effects
     drop.minted += amount;
