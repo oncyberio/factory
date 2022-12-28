0aea7932596433a66ff58051ce15464b025280bd
Rayan Boutaleb <rayan@oncyber.io>
2022-12-28 15:04:26 +0100

    W hardcoded values instead of variables

--- packages/contracts/contracts/Diamantaires/DestinationUtility/CyberDestinationUtilityFactoryFacet.sol
+++ packages/contracts/contracts/Diamantaires/DestinationUtility/CyberDestinationUtilityFactoryFacet.sol
@@ -8,25 +8,20 @@ abstract contract LuxContract {
 }
 
 contract CyberDestinationUtilityFactoryFacet is CyberDropBase {
-    address lootAddress = 0x3e96436B51B49d6f4dA8D4a0Bd4817Cd1F1719E0; // 0xb7be4001bff2c5f4a61dd2435e4c9a19d8d12343 in main
-    uint256 lootId = 1;
-    uint256 spacePodId = 1; // 5 in main
-
-    address luxAddress = 0xf763DB874F2A6014440D0F87F39D8A2B38801a31;
 
     function burnTransfer(uint256 amount) public {
         bool isLux = amount == 20;
         require(amount == 2 || isLux, 'WA');
 
-        ERC1155 lootPodContract = ERC1155(lootAddress);
-        require(lootPodContract.balanceOf(msg.sender, lootId) >= amount, 'NR');
-        require(balanceOf(msg.sender, spacePodId) >= amount, 'NS');
+        ERC1155 lootPodContract = ERC1155(0x3e96436B51B49d6f4dA8D4a0Bd4817Cd1F1719E0);
+        require(lootPodContract.balanceOf(msg.sender, 1) >= amount, 'NR');
+        require(balanceOf(msg.sender, 1) >= amount, 'NS');
         
-        _burn(msg.sender, spacePodId, amount);
-        lootPodContract.safeTransferFrom(msg.sender, 0x000000000000000000000000000000000000dEaD, lootId, amount, '');
+        _burn(msg.sender, 1, amount);
+        lootPodContract.safeTransferFrom(msg.sender, 0x000000000000000000000000000000000000dEaD, 1, amount, '');
 
         // mint new contract
-        LuxContract luxPodContract = LuxContract(luxAddress);
+        LuxContract luxPodContract = LuxContract(0xf763DB874F2A6014440D0F87F39D8A2B38801a31);
         luxPodContract.mint(msg.sender, isLux ? 2 : 1, 1);
     }
 }
